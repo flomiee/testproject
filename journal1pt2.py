c43904a97338ab51de6efb136ae6af1e943ae5ee
flomiee <nksanbor@ucsc.edu>
2026-01-11 12:34:34 -0800

    journal1pt2.py

--- journal1pt2.py
+++ journal1pt2.py
@@ -1,10 +1,12 @@
-x=5 #define integer x equal to 5
-y=4
-print('x+y=',x+y)
-print('x-y=',x-y)
+x = [0,0,3.2,5.3,2.5,3.6]
+print(type(x))
+print('x is',x)
 
-x=5
-y=2.5
-print('x+y=',x+y)
-print('x-y=',x-y)
+y = 3.2
+w = 5.3
+print('y+w=',y+w)
+print('y-w=',y-w)
 
+y = 3.2
+v = 2
+print('y+v',y+v)
